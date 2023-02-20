2b59fbaab74312ac0877a83ea3bf873c40f7c6f8
ParLelya <parlelya@vk.com>
2023-02-20 19:37:46 +0300

    Moved json to mockapi + add skeleton loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sorting from './components/Sorting';
 import Card from './components/Card';
-import pizzas from "./pizzas.json"
+import MyLoader from './components/MyLoader';
 
 const App: React.FC = () => {
-	
+	const [items, setItems] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		fetch('https://63f38bd1de3a0b242b445773.mockapi.io/items')
+			.then((response) => { response.json() })
+			.then((data: any) => {
+				setTimeout(() => {
+					setItems(data)
+					setIsLoading(false)
+				}, 1000)
+			})
+	}, [])
+
 	return (
 		<div className="wrapper">
 			<Header />
@@ -20,9 +33,9 @@ const App: React.FC = () => {
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
 						{
-							pizzas.map((obj) => {
-								return <Card {...obj} key={obj.id}/>
-							})
+							isLoading
+								? [...new Array(6)].map((_, index) => <MyLoader key={index} />)
+								: (items.map((obj: any) => <Card {...obj} key={obj.id} />))
 						}
 					</div>
 				</div>
